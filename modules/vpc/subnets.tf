5052747034a8bcd165a0dd3e586cd8d441da9e4f
Shalom Daniel <shalomd498@gmail.com>
2024-10-07 23:04:59 -0400

    inital infra changes after testing locally

---
+++ modules/vpc/subnets.tf
@@ -0,0 +1,26 @@
+resource "aws_subnet" "public" {
+  count                   = 3
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = local.public_subnets[count.index]
+  availability_zone       = local.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.environment}-public-subnet-${count.index + 1}-${random_string.suffix.result}"
+    Environment = var.environment
+    Type        = "Public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = 3
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = local.private_subnets[count.index]
+  availability_zone = local.availability_zones[count.index]
+
+  tags = {
+    Name        = "${var.environment}-private-subnet-${count.index + 1}-${random_string.suffix.result}"
+    Environment = var.environment
+    Type        = "Private"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "profile" {
+  description = "The AWS profile to use. (NEU)"
+  type        = string
+}
