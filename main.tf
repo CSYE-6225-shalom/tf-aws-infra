3c2f5ef55f6883c72f2057d89bf3b294aab4b462
Shalom Daniel <shalomd498@gmail.com>
2024-10-10 12:58:25 -0400

    all files now formatted

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  region      = var.region
-  environment = var.environment
-  vpc_cidr    = var.vpc_cidr
-  public_route_cidr = var.public_route_cidr
-  public_subnet_count = var.public_subnet_count
+  region               = var.region
+  environment          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  public_route_cidr    = var.public_route_cidr
+  public_subnet_count  = var.public_subnet_count
   private_subnet_count = var.private_subnet_count
 }
