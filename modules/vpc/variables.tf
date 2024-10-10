6f3b6601d3f37fbea861aa94950d40e9192f595e
Shalom Daniel <shalomd498@gmail.com>
2024-10-10 12:44:18 -0400

    remove defaults

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -16,17 +16,14 @@ variable "vpc_cidr" {
 variable "public_route_cidr" {
   description = "CIDR block for the public route"
   type        = string
-  default     = "0.0.0.0/0"
 }
 
 variable "public_subnet_count" {
   description = "Number of public subnets"
   type        = number
-  default     = 3
 }
 
 variable "private_subnet_count" {
   description = "Number of private subnets"
   type        = number
-  default     = 3
 }
