999a691ddabfcdfab1fe9b4c61d8420ab49394a3
Shalom Daniel <shalomd498@gmail.com>
2024-10-14 16:48:31 -0400

    ec2 config changes

---
+++ modules/ec2/main.tf
@@ -0,0 +1,66 @@
+resource "aws_security_group" "app_sg" {
+  name        = "${var.environment}-app-sg"
+  vpc_id      = var.vpc_id
+  description = "Security group for web applications"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.environment}-app-sg"
+    Environment = var.environment
+  }
+}
+
+resource "aws_instance" "app_instance" {
+  count                  = var.ec2_instance_count
+  ami                    = local.latest_ami_id
+  instance_type          = var.instance_type
+  subnet_id              = tolist(local.public_subnet_ids)[count.index]
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  root_block_device {
+    volume_size           = var.ec2_volume_size
+    volume_type           = var.ec2_volume_type
+    delete_on_termination = var.ec2_delete_on_termination
+  }
+
+  disable_api_termination = var.ec2_disable_api_termination
+
+  tags = {
+    Name        = "${var.environment}-app-instance-${count.index + 1}"
+    Environment = var.environment
+  }
+}
--- modules/vpc/locals.tf
+++ modules/vpc/locals.tf
@@ -4,6 +4,6 @@ data "aws_availability_zones" "available" {
 
 locals {
   availability_zones = data.aws_availability_zones.available.names
-  public_subnets     = [for i in range(3) : cidrsubnet(var.vpc_cidr, 4, i)]
-  private_subnets    = [for i in range(3) : cidrsubnet(var.vpc_cidr, 4, i + 3)]
+  public_subnets     = [for i in range(var.public_subnet_count) : cidrsubnet(var.vpc_cidr, 4, i)]
+  private_subnets    = [for i in range(var.private_subnet_count) : cidrsubnet(var.vpc_cidr, 4, i + var.public_subnet_count)]
 }
