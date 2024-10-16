ffaef930dd0f8e7b0c2ed672827a164acb1b58e1
Shalom Daniel <shalomd498@gmail.com>
2024-10-16 17:51:42 -0400

    configure data block to dynamically pull amis

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -18,8 +18,8 @@ variable "app_port" {
   type        = number
 }
 
-variable "ami_id" {
-  description = "AMI ID"
+variable "ami_owner_id" {
+  description = "AMI Owner ID"
   type        = string
 }
 
